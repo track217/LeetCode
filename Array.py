bce667fc0c9f97f736cfbd6e4d4f1347d9636ea8
track217 <412641046@qq.com>
2017-07-06 16:30:49 +0800

    Create Array.py

---
+++ Array.py
@@ -0,0 +1,12 @@
+268. Missing Number
+在一个包含0, 1, 2, ..., n的数组中，找到缺失的那个数，只有一个
+例子： nums = [0, 1, 3] return 2.
+要求： 线性复杂度
+
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return len(nums)*(len(nums)+1)/2- sum(nums)
